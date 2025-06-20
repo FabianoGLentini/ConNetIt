234bf4c707cbadcdf236313554495ee4a936c762
FabiaonGLentini <fabiano.g.lentini@gmail.com>
2025-06-20 15:56:15 -0700

    Add: nav html and icon img

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 const NavBar = () => {
   return (
     <div>
+      <div>
+        <h2>Account Name</h2>
+        <h4>Network</h4>
+      </div>
+      <span>Add Connection +</span>
       <button className="">
-        <img src="" alt="" />
+        <Image
+          src="/images/main/icons/burger-icon.svg"
+          alt="menu"
+          width={24}
+          height={24}
+        />
       </button>
     </div>
   );
