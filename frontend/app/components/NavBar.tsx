ca113453f13b2415bbe0eb9a07d2a079a18782f7
FabiaonGLentini <fabiano.g.lentini@gmail.com>
2025-06-28 14:45:18 -0700

    Add: network component quick todo's & rought set up

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Image from "next/image";
+//  TODO create package import for nav bar maybe???
 
 const NavBar = () => {
   return (
+    // TODO revise div look into using that<></> maybe?
     <div>
       <div>
         <h2>Account Name</h2>
---
+++ frontend/app/components/Network.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// TODO import card display fn
+// TODO import img for edit option
+
+const Network = () => {
+  return (
+    // TODO card display, so call opon car method and lay it out vertically in fix sizing?
+
+    // TODO figure out best way to set evnet listener for each card expand click and hoever effect
+    <div>
+      {/* TODO call card components to create connections display list*/}
+    </div>
+  );
+};
+
+export default Network;
