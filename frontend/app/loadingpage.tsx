9da8bc8b83c14e313fe8a4a61eb6eae642faec4b
FabiaonGLentini <fabiano.g.lentini@gmail.com>
2025-06-28 16:00:12 -0700

    Add: loading page rough set-up & todo's

---
+++ frontend/app/loadingpage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoadingPage() {
+  // TODO loading page set up
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,22 @@
 import Network from "./components/Network";
 import NavBar from "./components/NavBar";
 import SearchFilter from "./components/SearchFilter";
+import ConnectionInfoDisplay from "./components/ConnectionInfoDisplay";
 
 export default function Home() {
   return (
     // TODO check if using fragment best practice
     // TODO how to properly lay out each section
     <div>
+      {/* TODO left section */}
+      <SearchFilter />
+
+      {/* TODO mid section */}
       <NavBar />
       <Network />
-      <SearchFilter />
+
+      {/* TODO right section */}
+      <ConnectionInfoDisplay />
     </div>
   );
 }
