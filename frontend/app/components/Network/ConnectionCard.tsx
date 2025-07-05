ae381b5df5765346e8de7d0120dcf99f45b97a71
FabianoGLentini <fabiano.g.lentini@gmail.com>
2025-07-05 15:37:55 -0400

    Add: sandbox for card creation tmp

--- frontend/app/components/Network/ConnectionCard.tsx
+++ frontend/app/components/Network/ConnectionCard.tsx
@@ -4,54 +4,41 @@ import React from "react";
 const ConnectionCard = () => {
   return (
     // TODO remove tmp's and properly add var
-    <li className="list-row">
-      <div>
-        <img
-          className="size-10 rounded-box"
-          src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-        />
-      </div>
-      <div>
-        <div>Dio Lupa</div>
-        <div className="text-xs uppercase font-semibold opacity-60">
-          Remaining Reason
+    <div>
+      {/* top section of card */}
+      {/* TODO check div or section mos aproppriate tag */}
+      <section>
+        <h3>
+          NameTMP
+          {/* Name var here */}
+        </h3>
+        {/* TODO may remove Edit icon potentially */}
+        <button>
+          <img src="" alt="TMP edit icon" />
+        </button>
+      </section>
+
+      {/* Mid Section */}
+      <section>
+        {/* TODO revise approach for displaying */}
+        <div>
+          <img src="" alt="profile img TMP" />
+          <span>Company TMP</span>
+          <span>Job Tittle TMP</span>
         </div>
-      </div>
-      <button className="btn btn-square btn-ghost">
-        <svg
-          className="size-[1.2em]"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <g
-            strokeLinejoin="round"
-            strokeLinecap="round"
-            strokeWidth="2"
-            fill="none"
-            stroke="currentColor"
-          >
-            <path d="M6 3L20 12 6 21 6 3z"></path>
-          </g>
-        </svg>
-      </button>
-      <button className="btn btn-square btn-ghost">
-        <svg
-          className="size-[1.2em]"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <g
-            strokeLinejoin="round"
-            strokeLinecap="round"
-            strokeWidth="2"
-            fill="none"
-            stroke="currentColor"
-          >
-            <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-          </g>
-        </svg>
-      </button>
-    </li>
+      </section>
+
+      {/* Bottom Section */}
+      <section>
+        {/* Tag list */}
+        <ul>{/* TODO add call to tag icon creation */}</ul>
+
+        {/* TODO expand or indicate if more tags then space on card to be displayed ...?*/}
+        <button>
+          <img src="" alt="TMP expand tag icon" />
+        </button>
+      </section>
+    </div>
   );
 };
 
