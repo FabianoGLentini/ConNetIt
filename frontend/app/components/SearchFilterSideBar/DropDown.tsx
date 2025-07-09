c54991c81290e169fbacd8827f203682d6c7794d
FabianoGLentini <fabiano.g.lentini@gmail.com>
2025-07-09 14:37:48 -0400

    Add: rotate transition pointer for filter dropdown options

--- frontend/app/components/SearchFilterSideBar/DropDown.tsx
+++ frontend/app/components/SearchFilterSideBar/DropDown.tsx
@@ -9,7 +9,26 @@ const DropDown = () => {
         className="flex justify-between w-full h-fit cursor-pointer align-baseline"
       >
         <span>Filter Section TMP</span>
-        {accordionOpen ? (
+        <span
+          className={` transition-all flex justify-center ${accordionOpen ? "rotate-90" : ""}`}
+        >
+          <svg
+            width="8"
+            viewBox="0 0 11 19"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.85324 16.6636L9.03153 9.48531L1.85324 2.30702"
+              stroke="#333333"
+              stroke-width="3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </span>
+
+        {/* {accordionOpen ? (
           <span>tmp -</span>
         ) : (
           <span className="flex justify-center">
@@ -28,7 +47,7 @@ const DropDown = () => {
               />
             </svg>
           </span>
-        )}
+        )} */}
       </button>
 
       <ul
