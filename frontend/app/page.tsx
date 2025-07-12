af1fbd22226076b50727b0db355ab82184d72165
FabianoGLentini <fabiano.g.lentini@gmail.com>
2025-07-12 13:43:18 -0400

    WIP: json placeholder set up

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,15 +2,20 @@ import Network from "./components/layout/Network";
 import NavBar from "./components/layout/NavBar";
 import SearchFilterSideBar from "./components/layout/SearchFilterSideBar";
 import ConnectionInfoDisplay from "./components/layout/ConnectionInfoDisplay";
+// TODO once I have some basic understanding of using jsonplaceholder I can look into better hookup with my own java backend
+// import Connection from "../../backend/src/main/model/Network.java";
+import Connection from "@/app/types/Connection";
 
 interface User {
-    
+  // TODO tmp User set up for testing purposes
+  id: string;
+  connections: Connection[];
 }
 
 export default async function Home() {
   // TODO confirm that this is appropriate set up for fetch
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
+  const users: User[] = await res.json();
 
   return (
     // TODO check if using fragment best practice
